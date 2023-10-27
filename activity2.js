c7d1f2481448221f84a3f9e726e2fe8927a85cb1
Yoshini13 <118658957+Yoshini13@users.noreply.github.com>
2023-10-27 13:04:51 +0300

    Add files via upload

---
+++ activity2.js
@@ -0,0 +1,7 @@
+function getscore(){
+    score=localStorage.getItem("score");
+    document.getElementById("score").innerHTML="score:"+score;
+}
+function back(){
+    window.location="activity_1.html";
+}
